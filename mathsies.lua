4ef322a0115cdc710cf09af985a3aae0ff02f84e
Henry Fleminger Thomson <henryfthomson@gmail.com>
2018-12-05 14:05:16 +0000

    Added integrals, a few other functions and optimised some pieces. Also added metadata to the lbirary.

--- mathsies.lua
+++ mathsies.lua
@@ -1,6 +1,19 @@
--- math. Deterministic LuaJIT maths library.
--- By Tachytaenius.
--- MIT license.
+local metadata = {
+	name = "Mathsies",
+	version = "0.1.0",
+	author = "Tachytaenius",
+	license = [[
+		MIT License
+		
+		Copyright (c) 2018 Henry Fleminger Thomson
+		
+		Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+		
+		The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	]]
+}
 
 -- TODO: Logarithms
 
@@ -39,20 +52,23 @@ local function atan(x)
 	return asin(x/sqrt(1+x^2))
 end
 
+local function cot(x)
+	return cos(x)/sin(x)
+end
+
+local function acot(x)
+	return acos(x/sqrt(1+x^2))
+end
+
 local function distance(x, y)
 	return sqrt(x^2 + y^2)
 end
 
 local function angle(x, y)
-	if x == 0 then
-		x = 1
-	end
+	x = x == 0 and 1 or x
 	local a = atan(y/x)
-	if x < 0 then
-		a = a + tau/2
-	end
-	local theta = a % tau
-	return theta
+	a = x < 0 and a + tau / 2 or a
+	return a % tau
 end
 
 local function cartesianToPolar(x, y)
@@ -71,18 +87,11 @@ local function round(x, y)
 end
 
 local function sgn(x)
-	if x > 0 then 
-		return 1
-	elseif x == 0 then 
-		return 0
-	else
-		-- Did you hear about the mathematician who was afraid of negative numbers?
-		-- He'd stop at nothing to avoid them.
-		return -1
-		
-		-- Did you laugh at that? I hope you didn't, because zero is a number; the empty set is when things get nothingy.
-		-- "Zero is the cardinality of nothing, therefore zero is nothing." As if.
-	end
+	-- Did you hear about the mathematician who was afraid of negative numbers?
+	-- He'd stop at nothing to avoid them.
+	return x == 0 and 0 or abs(x) / x
+	-- Did you laugh at that? I hope you didn't, because zero is a number; the empty set is when things get nothingy.
+	-- "Zero is the cardinality of nothing, therefore zero is nothing." As if.
 end
 
 local function isInteger(x)
@@ -96,11 +105,41 @@ local function closestPointOnCircumference(px, py, cx, cy, r)
 	return ax, ay -- return the answer
 end
 
+local function int(a, b, f, n, i, ...)
+	n = n or 256
+	i = i or 1
+	if a > b then
+		a, b =  b, a
+	end
+	local sum = 0
+	table.insert(arg, i, _)
+	for v = a, b - (b - a) / n, (b - a) / n do
+		arg[i] = v
+		sum = sum + f(unpack(arg)) * (b - a) / n
+	end
+	print(i, n, sum)
+	return sum
+end
+
 local function tri(x)
 	return abs((x-tau/4)%tau-tau/2)/(tau/4)-1
 end
 
+local function finite(x)
+	return x == x and abs(x) ~= huge
+end
+
+local function isNan(x)
+	return x ~= x
+end
+
+local function isInfinite(x)
+	return abs(x) == huge
+end
+
 return {
+	metadata = metadata,
+	
 	tau = tau,
 	pi = tau / 2, -- LuaJIT will optimise "tau / 2" in code, so choose whichever you find personally gratifying. I use tau in this library.
 	sin = sin,
@@ -109,6 +148,9 @@ return {
 	acos = acos,
 	tan = tan,
 	atan = atan,
+	cot = cot,
+	acot = acot,
+	int = int,
 	distance = distance,
 	angle = angle,
 	cartesianToPolar = cartesianToPolar,
@@ -125,6 +167,7 @@ return {
 	min = min,
 	huge = huge,
 	tri = tri,
+	finite = finite,
 	
 	-- non-deterministic but faster, for use in graphics
 	ndCos = math.cos,
